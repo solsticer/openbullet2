b7557933dbc24dea46b7e223c5ea97cb38abc5da
nero <kvshcz@gmail.com>
2026-01-28 23:34:11 -0300

    Add `Random GUID` and move General functions to Miscellaneous
    Add the function block that generates a random GUID using built-in
    provider. Additionally, move the General Purpose functions to the
    Miscellaneous group to make it more cohesive and easier to find.

---
+++ RuriLib/Blocks/Functions/Miscellaneous/Methods.cs
@@ -0,0 +1,46 @@
+﻿using RuriLib.Attributes;
+using RuriLib.Models.Bots;
+using RuriLib.Providers.UserAgents;
+using System;
+
+namespace RuriLib.Blocks.Functions.Miscellaneous
+{
+    [BlockCategory("Miscellaneous", "General purpose functions", "#9acd32")]
+    public static class Methods
+    {
+        [Block("Generates a random User Agent using the builtin provider or a custom list")]
+        public static string RandomUserAgent(BotData data, UAPlatform platform = UAPlatform.All)
+        {
+            data.Logger.LogHeader();
+            data.Logger.Log("Getting random UA from the builtin provider");
+            string ua;
+
+            try
+            {
+                ua = data.Providers.RandomUA.Generate(platform);
+            }
+            catch
+            {
+                ua = "NO_RANDOM_UA_FOUND";
+            }
+
+            data.Logger.Log(ua);
+            return ua;
+        }
+
+        [Block("Generates a random GUID using the builtin provider",
+            extraInfo = "Full list of formats here: https://learn.microsoft.com/en-us/dotnet/api/system.guid.tostring under the 'Remarks' section")]
+        public static string RandomGuid(BotData data, string format = "D", bool KeepDashes = true)
+        {
+            data.Logger.LogHeader();
+            data.Logger.Log("Getting random GUID from the builtin provider");
+
+            var guid = Guid.NewGuid().ToString(format);
+            if (!KeepDashes)
+                guid = guid.Replace("-", "");
+            data.Logger.Log(guid);
+
+            return guid;
+        }
+    }
+}
